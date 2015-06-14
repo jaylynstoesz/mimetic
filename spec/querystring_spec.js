e9725070b6307b7d73f2d5cc07ec5415d68697a4
Jaylyn Stoesz <jaylynstoesz@gmail.com>
2015-06-14 15:16:17 -0600

    Stoesz mimetic exercise

--- spec/querystring_spec.js
+++ spec/querystring_spec.js
@@ -1,7 +1,8 @@
-var parseQueryString = require('../lib/querystring');
+var file = require('../lib/querystring');
 
 describe('parseQueryString', function () {
   it('turns a querystring into an object', function () {
+
 
     // Part 1: setup your input
     var input = '?artist=Miles&year=1948';
@@ -9,7 +10,7 @@ describe('parseQueryString', function () {
     // Part 2: call your function,
     //    passing in the input
     //    and capturing the result in a variable named `actual`
-    var actual = parseQueryString(input);
+    var actual = file.parseQueryString(input);
 
     // Part 3a: define your expected output for the given input
     var expected = { artist: 'Miles', year: '1948' }
@@ -18,4 +19,13 @@ describe('parseQueryString', function () {
     expect(actual).toEqual(expected);
 
   });
+
+
+it("returns empty object when input is null", function() {
+  var input = null;
+  var actual = file.parseQueryString(input);
+  var expected = {};
+  expect(actual).toEqual(expected);
+  });
+
 });
